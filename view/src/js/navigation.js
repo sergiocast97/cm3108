90bb0e8b5e5bdf13ab971c8ef4c5d07c6b6c2e10
Thomas Sutherland <23324165+Tssomas@users.noreply.github.com>
2019-10-19 14:07:25 +0100

    Add files via upload

---
+++ view/src/js/navigation.js
@@ -0,0 +1,16 @@
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("side_nav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("side_nav").style.width = "0";
+}
+
+/* Set the navigation bar title when a menu item is clicked */
+function setTitle(title) {
+    document.getElementById("title").innerHTML = title;
+    // Menu item must have been clicked, close the menu
+    closeNav()
+}
