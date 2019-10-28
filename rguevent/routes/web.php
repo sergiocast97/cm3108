1b07e6af05ae6c4be76d66846e7807e07d2354a9
sergiocast97 <sergiocast97@gmail.com>
2019-10-28 15:32:35 +0000

    Removed Event Manager

--- rguevent/routes/web.php
+++ rguevent/routes/web.php
@@ -19,10 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/eventmanager', function () {
-    return view('eventmanager');
-});
-
 Route::get('/admin', 'UserController@user');
 
 Route::get('/event', function() {
