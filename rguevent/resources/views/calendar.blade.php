d2ffa25613b392c03e412dbf4c66b03b2c6eaf69
sergiocast97 <sergiocast97@gmail.com>
2019-11-03 01:39:20 +0000

    Calendar styling

--- rguevent/resources/views/calendar.blade.php
+++ rguevent/resources/views/calendar.blade.php
@@ -2,23 +2,23 @@
 @section('content')
 
 <!-- Calendar area -->
-<div class="calendar_area">
-
-    <div class="calendar-tools">
-        <div id="back-btn">
-            <button id="change-back">&lt;</button>
-        </div>
-        <div id="calendar-current">
-            <span id="display-month"></span>
-            <span id="display-year"></span>
-        </div>
-        <div id="forward-btn">
-            <button id="change-forward">&gt;</button>
-        </div>
-    </div>
+<div class="main_area calendar_area">
 
     <!-- Calendar -->
     <div class="calendar">
+        
+        <div class="calendar-tools">
+            <div id="back-btn">
+                <button id="change-back">&lt;</button>
+            </div>
+            <div id="calendar-current">
+                <span id="display-month"></span>
+                <span id="display-year"></span>
+            </div>
+            <div id="forward-btn">
+                <button id="change-forward">&gt;</button>
+            </div>
+        </div>
         
         <!-- Monthly Calendar -->
         <table id="cal-month">
@@ -29,9 +29,9 @@
 </div>
 
 <!-- Events Area -->
-<div class="events_area">
+<div class="side">
 
-    <div class="events_day" id="selected-date">Today</div>
+    <div class="side_title" id="selected-date">Today</div>
 
     <!-- Event List -->
     <div class="events_list">
@@ -58,11 +58,11 @@
 
     <div class="single_footer">
         <div class="single_time">
-            <img src="{{ URL::asset('img/time.svg') }}" class="event_icon" alt="clock">
+            <img src="{{ URL::asset('img/time_black.svg') }}" class="event_icon" alt="clock">
             <span></span>
         </div>
         <div class="single_location">
-            <img src="{{ URL::asset('img/location.svg') }}" class="event_icon" alt="location">
+            <img src="{{ URL::asset('img/location_black.svg') }}" class="event_icon" alt="location">
             <span></span>
         </div>
     </div>
